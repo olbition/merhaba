e9a9f60e43b9d5092745939e214882bbb2093e3a
Anıl Batuhan ASLAN <103073156+olbition@users.noreply.github.com>
2024-02-20 16:30:01 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+'use client'
+import { useState } from "react";
+export default function Home() {
+
+  const [anil,setAnil] = useState("DEnememeeeemememememem")
+
+  return (
+    <>
+        <main>
+          <h1>{anil}</h1>
+        </main>
+    </>
+  );
+}
